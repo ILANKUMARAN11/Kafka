d5950404b0f71ae2ec8742ead533c820cd319ef8
ilankumaran.ilangovan <ilankumaran.ilangovan@sanrussoft.com>
2022-04-29 22:21:19 +0100

    test

---
+++ Kafka-Example/Kafka-Producer/src/main/java/com/ilan/config/KafkaProperties.java
@@ -0,0 +1,35 @@
+package com.ilan.config;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+
+@Configuration
+@Order(-1)
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class KafkaProperties {
+
+    @Value("${kafka.consumer.bootstrap-servers}")
+    private String consumerBootstrapServers;
+
+    @Value("${kafka.producer.bootstrap-servers}")
+    private String producerBootstrapServers;
+
+    @Value("${kafka.simple.topic-name}")
+    private String simpleTopicName;
+
+    @Value("${kafka.simple.group-id}")
+    private String simpleGroupId;
+
+    @Value("${kafka.employee.topic-name}")
+    private String employeeTopicName;
+
+    @Value("${kafka.employee.group-id}")
+    private String employeeGroupId;
+
+}
