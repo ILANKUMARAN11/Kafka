6ecf144d30fd1804042f86ae26d0c304f4bb679a
ilankumaran.ilangovan <ilankumaran.ilangovan@sanrussoft.com>
2023-06-06 14:51:08 +0100

    new way to generate schema

--- PojoToAvro-AvroToPojo/src/main/java/MainRunner.java
+++ PojoToAvro-AvroToPojo/src/main/java/MainRunner.java
@@ -1,32 +1,61 @@
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.fasterxml.jackson.annotation.PropertyAccessor;
 import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.MapperFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.dataformat.avro.AvroFactory;
+import com.fasterxml.jackson.dataformat.avro.AvroMapper;
 import com.fasterxml.jackson.dataformat.avro.AvroSchema;
+import com.fasterxml.jackson.dataformat.avro.jsr310.AvroJavaTimeModule;
 import com.fasterxml.jackson.dataformat.avro.schema.AvroSchemaGenerator;
 import my.pojo.Employee;
+import pojo.Earth;
+import pojo.Mars;
+import pojo.Universe;
 
 import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 
 public class MainRunner {
 
 
     public static void main(String ilan[]) throws IOException {
-        ObjectMapper mapper = new ObjectMapper(new AvroFactory());
-        mapper.setVisibility(PropertyAccessor.FIELD, JsonAutoDetect.Visibility.ANY);
+//        ObjectMapper mapper = new ObjectMapper(new AvroFactory());
+//        mapper.setVisibility(PropertyAccessor.FIELD, JsonAutoDetect.Visibility.ANY);
+//        AvroSchemaGenerator gen = new AvroSchemaGenerator();
+//        mapper.acceptJsonFormatVisitor(Employee.class, gen);
+
+
+        AvroMapper avroMapper = AvroMapper.builder()
+                .enable(MapperFeature.SORT_PROPERTIES_ALPHABETICALLY)
+                .enable(MapperFeature.APPLY_DEFAULT_VALUES)
+                //.disable(MapperFeature.SORT_PROPERTIES_ALPHABETICALLY)
+                .addModule(new AvroJavaTimeModule())
+                .build();
+        createAvroSchemaFromClass(Employee.class, avroMapper, ".avro");
+
+        createAvroSchemaFromClass(Universe.class, avroMapper, ".avsc");
+        createAvroSchemaFromClass(Earth.class, avroMapper, ".avsc");
+        createAvroSchemaFromClass(Mars.class, avroMapper, ".avsc");
+
+    }
+
+
+    private static void createAvroSchemaFromClass(Class<?> clazz, AvroMapper avroMapper, String extention) throws IOException {
+
         AvroSchemaGenerator gen = new AvroSchemaGenerator();
-        mapper.acceptJsonFormatVisitor(Employee.class, gen);
+        gen.enableLogicalTypes();
+        avroMapper.acceptJsonFormatVisitor(clazz, gen);
         AvroSchema schemaWrapper = gen.getGeneratedSchema();
+
         org.apache.avro.Schema avroSchema = schemaWrapper.getAvroSchema();
-        String asJson = avroSchema.toString(true);
+        String avroSchemaInJSON = avroSchema.toString(true);
 
-
-        System.out.println(asJson);
-
-        Files.write(Paths.get("src/main/resources/Employee.avro"), asJson.getBytes());
+        //Write to File
+        Path fileName = Path.of("src/main/resources/"+clazz.getSimpleName() + extention);
+        Files.writeString(fileName, avroSchemaInJSON);
 
     }
 }
